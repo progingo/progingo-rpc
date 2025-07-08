f0b454a05ea2f21ef302c29eb403be0b074ff6a2
progingo <progingo@qq.com>
2025-07-08 14:41:53 +0800

    新增全局配置、全局配置对象 使用双检锁单例模式获取配置对象(单例模式)

---
+++ example-consumer/src/main/java/org/progingo/consumer/ConsumerExample.java
@@ -0,0 +1,18 @@
+package org.progingo.consumer;
+
+import org.progingo.config.RpcConfig;
+import org.progingo.utils.ConfigUtils;
+
+/**
+ * 简易服务消费者示例
+ *
+ */
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+/*        //测试能不能获取到配置文件
+        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        System.out.println(rpc);*/
+
+    }
+}
--- example-provider/src/main/java/org/progingo/provider/EasyProviderExample.java
+++ example-provider/src/main/java/org/progingo/provider/EasyProviderExample.java
@@ -1,5 +1,7 @@
 package org.progingo.provider;
 
+import org.progingo.config.RpcApplication;
+import org.progingo.config.RpcConfig;
 import org.progingo.registry.LocalRegistry;
 import org.progingo.server.HttpServer;
 import org.progingo.server.VertxHttpServer;
@@ -10,6 +12,11 @@ import org.progingo.service.UserService;
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
+        // RPC 框架初始化
+        RpcApplication.init();
+        //尝试获取配置类
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        System.out.println("RPC配置：" + rpcConfig);
         // 注册服务
         LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
         // 启动 web 服务
