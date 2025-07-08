b6ffeb51c07ddc72f156d96d2d08915849f4203f
progingo <progingo@qq.com>
2025-07-08 12:59:04 +0800

    添加本地注册器 添加序列化器 请求、响应封装类

--- example-provider/src/main/java/org/progingo/provider/EasyProviderExample.java
+++ example-provider/src/main/java/org/progingo/provider/EasyProviderExample.java
@@ -1,13 +1,17 @@
 package org.progingo.provider;
 
+import org.progingo.registry.LocalRegistry;
 import org.progingo.server.HttpServer;
 import org.progingo.server.VertxHttpServer;
+import org.progingo.service.UserService;
 
 /**
  * 简易服务提供者示例
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
--- progingo-rpc-esay/src/main/java/org/progingo/server/VertxHttpServer.java
+++ progingo-rpc-esay/src/main/java/org/progingo/server/VertxHttpServer.java
@@ -3,6 +3,11 @@ package org.progingo.server;
 import io.vertx.core.Vertx;
 
 public class VertxHttpServer implements HttpServer{
+    /**
+     * 启动服务器
+     *
+     * @param port
+     */
     public void doStart(int port) {
         // 创建 Vert.x 实例
         Vertx vertx = Vertx.vertx();
@@ -11,15 +16,7 @@ public class VertxHttpServer implements HttpServer{
         io.vertx.core.http.HttpServer server = vertx.createHttpServer();
 
         // 监听端口并处理请求
-        server.requestHandler(request -> {
-            // 处理 HTTP 请求
-            System.out.println("Received request: " + request.method() + " " + request.uri());
-
-            // 发送 HTTP 响应
-            request.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x HTTP server!");
-        });
+        server.requestHandler(new HttpServerHandler());
 
         // 启动 HTTP 服务器并监听指定端口
         server.listen(port, result -> {
