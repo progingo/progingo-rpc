c3082d572dc01ac3b89a07a6b32348a384be7726
progingo <progingo@qq.com>
2025-07-09 21:12:18 +0800

    添加黏包、半包实例

---
+++ progingo-rpc-core/src/main/java/org/progingo/tcp/PacketStickinessTestClient.java
@@ -0,0 +1,34 @@
+package org.progingo.tcp;
+
+import io.vertx.core.Vertx;
+
+/**
+ * 黏包半包测试消费类
+ */
+public class PacketStickinessTestClient {
+    public void start() {
+        // 创建 Vert.x 实例
+        Vertx vertx = Vertx.vertx();
+
+        vertx.createNetClient().connect(8888, "localhost", result -> {
+            if (result.succeeded()) {
+                System.out.println("Connected to TCP server");
+                io.vertx.core.net.NetSocket socket = result.result();
+                for (int i = 0; i < 1000; i++) {
+                    // 发送数据
+                    socket.write("Hello, server!Hello, server!Hello, server!Hello, server!");
+                }
+                // 接收响应
+                socket.handler(buffer -> {
+                    System.out.println("Received response from server: " + buffer.toString());
+                });
+            } else {
+                System.err.println("Failed to connect to TCP server");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new PacketStickinessTestClient().start();
+    }
+}
---
+++ progingo-rpc-core/src/main/java/org/progingo/tcp/PacketStickinessTestServer.java
@@ -0,0 +1,58 @@
+package org.progingo.tcp;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.net.NetServer;
+import org.progingo.server.HttpServer;
+
+/**
+ * 黏包半包测试服务类
+ */
+public class PacketStickinessTestServer implements HttpServer {
+        @Override
+        public void doStart(int port) {
+            // 创建 Vert.x 实例
+            Vertx vertx = Vertx.vertx();
+
+            // 创建 TCP 服务器
+            NetServer server = vertx.createNetServer();
+
+            // 处理请求
+//        server.connectHandler(new TcpServerHandler());
+            server.connectHandler(socket -> {
+                socket.handler(buffer -> {
+                    String testMessage = "Hello, server!Hello, server!Hello, server!Hello, server!";
+                    int messageLength = testMessage.getBytes().length;
+                    if (buffer.getBytes().length < messageLength) {
+                        System.out.println("半包, length = " + buffer.getBytes().length);
+                        System.out.println(new String(buffer.getBytes(0, buffer.getBytes().length)));
+                        return;
+                    }
+                    if (buffer.getBytes().length > messageLength) {
+                        System.out.println("粘包, length = " + buffer.getBytes().length);
+                        System.out.println(new String(buffer.getBytes(0, buffer.getBytes().length)));
+                        return;
+                    }
+                    String str = new String(buffer.getBytes(0, messageLength));
+                    //System.out.println(str);
+                    if (testMessage.equals(str)) {
+                        System.out.println("good");
+                    }
+                });
+            });
+
+            // 启动 TCP 服务器并监听指定端口
+            server.listen(port, result -> {
+                if (result.succeeded()) {
+                    System.out.println("TCP server started on port " + port);
+                    //log.info("TCP server started on port " + port);
+                } else {
+                    System.out.println("Failed to start TCP server: " + result.cause());
+                    //log.info("Failed to start TCP server: " + result.cause());
+                }
+            });
+        }
+
+        public static void main(String[] args) {
+            new PacketStickinessTestServer().doStart(8888);
+        }
+}
